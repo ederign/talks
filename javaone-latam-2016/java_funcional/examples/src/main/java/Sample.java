0d1ff3faaa8e6626748f4a517f2cc6d9c699828d
ederign <ignatowicz@gmail.com>
2016-06-26 19:11:06 -0300

    Keynote Sample

--- javaone-latam-2016/java_funcional/examples/src/main/java/Sample.java
+++ javaone-latam-2016/java_funcional/examples/src/main/java/Sample.java
@@ -1,8 +1,22 @@
+import com.sun.tools.javac.util.List;
+
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
 public class Sample {
+    /*
+
+        List<Integer> listOfNumbers = List.of(1, 2, 3);
+    System.out.println(listOfNumbers);
+
+    Set<Integer> setOfNumbers = Set.of(1, 2, 3);
+    System.out.println(setOfNumbers);
+
+    Map<String, String> mapOfString = Map.of("Key1", "Val1", "Key2", "Val2");
+    System.out.println(mapOfString);
+
+     */
 
 
     public static void main( String[] args ) {
@@ -12,6 +26,8 @@ public class Sample {
             ImageIcon icon = new ImageIcon( file.toURI().toURL() );
             JLabel label = new JLabel( icon );
             JOptionPane.showMessageDialog( null, label );
+
+
         } catch ( IOException e ) {
             e.printStackTrace();
         }
