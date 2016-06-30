072a0dce6a6d48f9a35525e20dd9eaa008876202
ederign <ignatowicz@gmail.com>
2016-06-30 18:04:22 -0300

    Java Funcional Sample and Slides

--- javaone-latam-2016/java_funcional/examples/src/main/java/Sample.java
+++ javaone-latam-2016/java_funcional/examples/src/main/java/Sample.java
@@ -1,35 +1,36 @@
-import com.sun.tools.javac.util.List;
-
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-
-public class Sample {
-    /*
-
-        List<Integer> listOfNumbers = List.of(1, 2, 3);
-    System.out.println(listOfNumbers);
-
-    Set<Integer> setOfNumbers = Set.of(1, 2, 3);
-    System.out.println(setOfNumbers);
-
-    Map<String, String> mapOfString = Map.of("Key1", "Val1", "Key2", "Val2");
-    System.out.println(mapOfString);
-
-     */
-
-
-    public static void main( String[] args ) {
-
-        try {
-            File file = new File( "/Users/ederign/javaone/surprise.gif" );
-            ImageIcon icon = new ImageIcon( file.toURI().toURL() );
-            JLabel label = new JLabel( icon );
-            JOptionPane.showMessageDialog( null, label );
-
-
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        }
-    }
-}
+//import com.sun.tools.javac.util.List;
+//
+//import javax.swing.*;
+//import java.io.File;
+//import java.io.IOException;
+//
+//public class Sample {
+//    /*
+//
+//        List<Integer> listOfNumbers = List.of(1, 2, 3);
+//    System.out.println(listOfNumbers);
+//
+//    Set<Integer> setOfNumbers = Set.of(1, 2, 3);
+//    System.out.println(setOfNumbers);
+//
+//    Map<String, String> mapOfString = Map.of("Key1", "Val1", "Key2", "Val2");
+//    System.out.println(mapOfString);
+//
+//     */
+//
+//
+//    public static void main( String[] args ) {
+//
+//        try {
+////            import javax.swing.*;
+////            File file = new File( "/Users/ederign/javaone/eder.png" );
+////            ImageIcon icon = new ImageIcon( file.toURI().toURL() );
+////            JLabel label = new JLabel( icon );
+////            JOptionPane.showMessageDialog( null, label );
+//
+//
+//        } catch ( IOException e ) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
